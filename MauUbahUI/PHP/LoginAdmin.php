f6400f843d3c4831ca725aeab8b31a797cb047c2
teukurkpp <raka19495@gmail.com>
2025-05-20 17:21:59 +0700

    Add files via upload

---
+++ MauUbahUI/PHP/LoginAdmin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KelasKom - Login Admin</title>
+    <link rel="stylesheet" href="../CSS/Login.css">
+
+</head>
+<body>
+    <div class="form-login">
+        <div class="login-card">
+          <form class="login-form">
+            <h2>Login</h2>
+            <label>Email</label>
+            <input type="email" placeholder="Masukkan Email Anda" required>
+            
+            <label>Kata Sandi</label>
+            <input type="password" placeholder="Masukkan Sandi Anda" required>
+
+            <p class="sandi">Lupa Sandi?</p>
+            
+            <a href="../HTML/PilihHari.html" class="btn-lanjut">Lanjut</a>
+            
+            <p class="option">Atau</p>
+            <button class="google-btn">Masuk Menggunakan Google</button>
+        
+            <a href="../PHP/LoginUser.php" class="link">Masuk sebagai Mahasiswa</a>
+          </form>
+        </div>
+    </div>    
+    
+</body>
+</html>
---
+++ MauUbahUI/PHP/LoginUser.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KelasKom - Login Mahasiswa</title>
+    <link rel="stylesheet" href="../CSS/Login.css">
+
+</head>
+<body>
+  
+  <div class="form-login">
+    <div class="login-card">
+      <form class="login-form">
+        <h2>Login</h2>
+        <label>Email</label>
+          <input type="email" placeholder="Masukkan Email Anda" required>
+            
+          <label>Kata Sandi</label>
+          <input type="password" placeholder="Masukkan Sandi Anda" required>
+
+          <p class="sandi">Lupa Sandi?</p>
+            
+          <a href="../HTML/PilihHari.html" class="btn-lanjut">Lanjut</a>
+            
+          <p class="option">Atau</p>
+          <button class="google-btn">Masuk Menggunakan Google</button>
+        
+          <a href="../PHP/LoginAdmin.php" class="link">Masuk sebagai Admin</a>
+      </form>
+    </div>
+  </div>    
+    
+</body>
+</html>
