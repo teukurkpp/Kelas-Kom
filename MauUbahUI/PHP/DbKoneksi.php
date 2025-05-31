d9648df06eb38139bc7c3e91ada6972f233b996d
teukurkpp <raka19495@gmail.com>
2025-05-31 23:11:14 +0700

    Delete MauUbahUI directory

--- MauUbahUI/PHP/DbKoneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "kelaskom");
-
-if (!$conn) {
-    die("konseki gagal: " . mysqli_connect_error());
-}
-
-?>
--- MauUbahUI/PHP/HapusJadwalKelas.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'DbJadwalKelas.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM jadwal_matkul WHERE id = $id");
-header("Location: TampilanJadwalKelas.php");
-?>
